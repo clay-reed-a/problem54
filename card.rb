702d2fe456bbbf36f92f29292a437a41deb90355
Clayton Albachten <clay.reed.a@gmail.com>
2015-11-05 13:00:19 -0500

    Write Card::consecutive_valued? to support Hand class.

--- card.rb
+++ card.rb
@@ -1,15 +1,24 @@
+require 'pry'
+
 class Card
   include Comparable
 
-  PIPS = %w[2 3 4 5 6 7 8 9 10]
+  PIPS = %w[2 3 4 5 6 7 8 9 T]
   FACES = %w[J Q K A]
   VALUES = PIPS + FACES
+
+  def self.consecutive_valued? cards
+    card_values = cards.map(&:rank)
+    VALUES.each_cons(5).any? do |section|
+      card_values == section
+    end
+  end
 
   attr_reader :suit, :rank
 
   def initialize card
-    @rank = card[0]
-    @suit = card[1]
+    @rank = parse_rank card
+    @suit = prase_suit card
   end
 
   def <=>(other)
@@ -23,4 +32,14 @@ class Card
   def value
     VALUES.index rank
   end
+
+    private
+
+    def prase_suit card
+      card[1]
+    end
+
+    def parse_rank card
+      card[0]
+    end
 end
--- hand.rb
+++ hand.rb
@@ -1,5 +1,29 @@
+require_relative 'card'
+
 class Hand
+
+  include Comparable
+
+  attr_reader :cards
+
   def initialize cards
-    @cards = cards
+    @cards = cards.map { |card| Card.new card }.sort
+  end
+
+  def value
+  end
+
+  def flush?
+    cards.map(&:suit).uniq == 1
+  end
+
+  def straight_flush?
+    flush? &&
+    Card.consecutive_valued?(cards)
+  end
+
+  def royal_flush?
+    flush? &&
+    (cards.map(&:rank) == %w[T J Q K A])
   end
 end
